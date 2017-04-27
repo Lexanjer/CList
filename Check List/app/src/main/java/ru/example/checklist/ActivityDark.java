8e616e8df30f23a7a7af6478436d4eec3fa81b36
Lexanjer <2078alex@gmail.com>
2017-04-27 23:31:35 +0400

    format ma

--- Check List/app/src/main/java/ru/example/checklist/ActivityDark.java
+++ Check List/app/src/main/java/ru/example/checklist/ActivityDark.java
@@ -1,39 +1,38 @@
 package ru.example.checklist;
 
-import android.os.*;
-import android.support.v4.view.*;
-import android.support.v7.app.*;
-import android.util.*;
-import android.widget.*;
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
-public class ActivityDark extends AppCompatActivity
-{
-		
-		final String LOG_TAG = "myLogs";
+public class ActivityDark extends AppCompatActivity {
 
-		static final String TAG = "myLogs";
-    static int PAGE_COUNT ;
+    final String LOG_TAG = "myLogs";
+
+    static final String TAG = "myLogs";
+    static int PAGE_COUNT;
 
     ViewPager pager;
     PagerAdapter pagerAdapter;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-		setTheme(R.style.AppDefault);
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_light);
 
-				DataBase dataBase = new DataBase(this);
-				Toast.makeText(this, "здесь выводится база "
-											 , Toast.LENGTH_LONG).show();
-				Log.d(TAG, "Vcem privet----------------- "+dataBase.getCheckList(1).toString() );
+        DataBase dataBase = new DataBase(this);
+        Toast.makeText(this, "здесь выводится база "
+                , Toast.LENGTH_LONG).show();
+        Log.d(TAG, "Vcem privet----------------- " + dataBase.getCheckList(1).toString());
 
 
         PAGE_COUNT = dataBase.getCount();
-				//   System.out.println(dataBase.getCheckList1().toString());
-				//   Log.d(TAG, "Vcem privet----------------- "+dataBase.getCheckList(1).toString() );
+        //   System.out.println(dataBase.getCheckList1().toString());
+        //   Log.d(TAG, "Vcem privet----------------- "+dataBase.getCheckList(1).toString() );
 
         pager = (ViewPager) findViewById(R.id.pager);
         method();
@@ -41,31 +40,30 @@ public class ActivityDark extends AppCompatActivity
 
         pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
 
-								@Override
-								public void onPageSelected(int position) {
-										Log.d(TAG, "onPageSelected, position = " + position);
-								}
+            @Override
+            public void onPageSelected(int position) {
+                Log.d(TAG, "onPageSelected, position = " + position);
+            }
 
-								@Override
-								public void onPageScrolled(int position, float positionOffset,
-																					 int positionOffsetPixels) {
-								}
+            @Override
+            public void onPageScrolled(int position, float positionOffset,
+                                       int positionOffsetPixels) {
+            }
 
-								@Override
-								public void onPageScrollStateChanged(int state) {
-								}
-						});
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
 
     }
 
-		
-		private void method()
-		{
-				pagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
-		}
+
+    private void method() {
+        pagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
+    }
 
 
-		// New coment
+    // New coment
 }
 
 
