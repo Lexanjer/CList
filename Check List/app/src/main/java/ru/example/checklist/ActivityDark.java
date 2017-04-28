1cab17de2596fed6a1b9795e0c149153f1018c53
Lexanjer <2078alex@gmail.com>
2017-04-28 23:18:06 +0400

    add activity dark continue

--- Check List/app/src/main/java/ru/example/checklist/ActivityDark.java
+++ Check List/app/src/main/java/ru/example/checklist/ActivityDark.java
@@ -1,64 +1,78 @@
 package ru.example.checklist;
 
-import android.os.*;
-import android.support.design.widget.*;
-import android.support.v4.view.*;
-import android.support.v4.widget.*;
-import android.support.v7.app.*;
-import android.util.*;
-import android.view.*;
-import android.widget.*;
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.PagerTitleStrip;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 
 public class ActivityDark extends AppCompatActivity {
 
     final String LOG_TAG = "myLogs";
 
     static final String TAG = "myLogs";
-   // static int PAGE_COUNT;
+    // static int PAGE_COUNT;
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private ViewPager viewPager;
 
-    ViewPager pager;
-    PagerAdapter pagerAdapter;
-		static int ptsTextSize;
+    private TabsFragmentAdapter adapter;
+    private ViewPager pager;
+    private PagerAdapter pagerAdapter;
+    static int ptsTextSize;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDark);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_light);
+        setContentView(R.layout.activity_dark);
 
-				
-					
-				DrawerLayout rootlayout = (DrawerLayout) findViewById(R.id.root_layout);
-				rootlayout.setBackgroundColor(R.color.colorPrimaryAppDark);
-			//	rootlayout.setTextC
-				
-				ImageView ramka = (ImageView) findViewById(R.id.imageView2);
-				ramka.setBackgroundColor(R.drawable.roundrect_layout_dark);
-				
-				CoordinatorLayout coorlayout = (CoordinatorLayout) findViewById(R.id.coordl2);
-				coorlayout.setBackgroundColor(R.drawable.roundrect);//_dark
-				
-				ImageView background = (ImageView) findViewById(R.id.imageBackground);
-				background.setBackgroundColor(R.drawable.bordo);
-				
-				PagerTitleStrip pts = (PagerTitleStrip) findViewById(R.id.titleStrip);
-				//ptsTextSize = 24;
-				pts.setBackgroundColor(R.drawable.roundrect_pager_dark);
-				pts.setTextColor(0xffffffff); //(R.color.colorWhite);
-			//	pts.setTextSize(ptsTextSize,);				
-				
-			
-			NavigationView navView = (NavigationView) findViewById(R.id.navigationView);
-			navView.setBackgroundColor(R.color.colorPrimaryAppDark);
-			
-			
-			//	initToolbar();
-			
+
+   /*     DrawerLayout rootlayout = (DrawerLayout) findViewById(R.id.root_layout);
+        rootlayout.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDarkAppDark));
+
+        //	rootlayout.setTextC
+
+        ImageView ramka = (ImageView) findViewById(R.id.imageView2);
+        ramka.setBackground(getResources().getDrawable(R.drawable.roundrect_layout_dark));
+
+        CoordinatorLayout coorlayout = (CoordinatorLayout) findViewById(R.id.coordl2);
+        coorlayout.setBackground(getResources().getDrawable(R.drawable.roundrect));//_dark
+
+        ImageView background = (ImageView) findViewById(R.id.imageBackground);
+        background.setImageDrawable(getResources().getDrawable(R.drawable.bordo));
+
+        PagerTitleStrip pts = (PagerTitleStrip) findViewById(R.id.titleStrip);
+        //ptsTextSize = 24;
+        pts.setBackgroundColor(R.drawable.roundrect_pager_dark);
+        pts.setTextColor(0xffffffff); //(R.color.colorWhite);
+        //	pts.setTextSize(ptsTextSize,);
+
+
+        NavigationView navView = (NavigationView) findViewById(R.id.navigationView);
+        navView.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDarkAppDark));
+
+*/
+        initToolbar();
+
+        PagerTitleStrip pts = (PagerTitleStrip) findViewById(R.id.titleStrip);
+        //ptsTextSize = 24;
+
+        pts.setTextColor(getResources().getColor(R.color.colorWhite)); //(R.color.colorWhite);
+        	pts.setTextSize((TypedValue.COMPLEX_UNIT_DIP), 24);
+
         DataBase dataBase = new DataBase(this);
         Toast.makeText(this, "здесь выводится база "
                 , Toast.LENGTH_LONG).show();
-        Log.d(TAG, "Vcem privet---------   Activity Dark  -------- " );
+        Log.d(TAG, "Vcem privet---------   Activity Dark  -------- ");
 
 
         MainActivity.PAGE_COUNT = dataBase.getCount();
@@ -66,9 +80,9 @@ public class ActivityDark extends AppCompatActivity {
         //   Log.d(TAG, "Vcem privet----------------- "+dataBase.getCheckList(1).toString() );
 
         pager = (ViewPager) findViewById(R.id.pager);
-       
-				pagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
-				
+
+        pagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
+
         pager.setAdapter(pagerAdapter);
 
         pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
@@ -90,20 +104,18 @@ public class ActivityDark extends AppCompatActivity {
 
     }
 
-		private void initToolbar() {
-      Toolbar  toolBar = (Toolbar) findViewById(R.id.toolBar);
-        toolBar.setTitle(R.string.app_name);
-		//		toolbar.setTextColor 
-        toolBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-								@Override
-								public boolean onMenuItemClick(MenuItem menuItem) {
-										return false;
-								}
-						});
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolBar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
 
-        toolBar.inflateMenu(R.menu.tb_menu);
+        toolbar.inflateMenu(R.menu.menu_dark);
     }
-    
 }
 
 
