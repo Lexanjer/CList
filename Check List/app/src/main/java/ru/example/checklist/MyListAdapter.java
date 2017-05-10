67aac15757c7721ee14e496e4051e7d7ce0d2cc7
Lexanjer <2078alex@gmail.com>
2017-05-10 17:25:33 +0400

    style cv

--- Check List/app/src/main/java/ru/example/checklist/MyListAdapter.java
+++ Check List/app/src/main/java/ru/example/checklist/MyListAdapter.java
@@ -1,15 +1,11 @@
 package ru.example.checklist;
 
-import android.content.Context;
-import android.graphics.Color;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import java.util.ArrayList;
+import android.content.*;
+import android.graphics.*;
+import android.support.v7.widget.*;
+import android.view.*;
+import android.widget.*;
+import java.util.*;
 
 //import android.support.v7.appcompat.R;
 
@@ -65,10 +61,12 @@ public class MyListAdapter extends ArrayAdapter<CheckItem> {
 		
 
     private class ViewHolder {
+				CardView cardV;
         final ImageView imageView;
         final TextView item, subitem;
 
         ViewHolder(View view) {
+						cardV = (CardView) view.findViewById(R.id.cardViewCheck);
             imageView = (ImageView) view.findViewById(R.id.imageView);
             item = (TextView) view.findViewById(R.id.itemView);
             subitem = (TextView) view.findViewById(R.id.subItemView);
